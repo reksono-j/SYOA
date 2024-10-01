3d0fa81f79c3a8260ebd2ea94ca104be719d7e4f
John-Rex <reksono.j@ufl.edu>
2024-10-01 01:24:15 -0400

    Implemented manager UIs

--- src/editor/parser/characterManager.py
+++ src/editor/parser/characterManager.py
@@ -11,24 +11,29 @@ class CharacterManager(QWidget):
 
         self.layout = QVBoxLayout()
 
+        # List Title
         self.label = QLabel("Character List")
         self.label.setAccessibleName("Character List")
         self.layout.addWidget(self.label)
 
+        # Area where characters are
         self.scrollArea = QScrollArea()
         self.scrollArea.setWidgetResizable(True)  
         self.scrollArea.setAccessibleName("Character list")
 
+        # Widget that holds a name
         self.nameContainer = QWidget()  
         self.nameLayout = QVBoxLayout(self.nameContainer)  
         self.scrollArea.setWidget(self.nameContainer)  
         self.layout.addWidget(self.scrollArea)
 
+        # Add button
         self.addButton = QPushButton("Add Name")
         self.addButton.setAccessibleName("Add Name Button")
         self.addButton.clicked.connect(self.addCharacter)
         self.layout.addWidget(self.addButton)
 
+        # Close button
         self.closeButton = QPushButton("Close")
         self.closeButton.setAccessibleName("Close Button")
         self.closeButton.clicked.connect(self.close)
@@ -38,13 +43,22 @@ class CharacterManager(QWidget):
         self.setWindowTitle("Character Manager")
 
         self.characters = []
-
+    
     def addCharacter(self):
         name = self.openNameDialog()
         if not name:
             return
-        self.characters.append(name)
-        
+        self.characters.append([name, []])
+        self.addAliasRow(name)
+    
+    def isPresent(self, name) -> bool:
+        if self.characters:
+            i = next((i for i, character in enumerate(self.characters) if character[0] == name), -1)
+            return i != -1
+        else:
+            return False
+    
+    def addAliasRow(self, name):
         nameLayout = QHBoxLayout()
         nameLabel = QLabel(name)
         nameLabel.setAccessibleName(f"Name: {name}")  
@@ -59,7 +73,12 @@ class CharacterManager(QWidget):
         nameLayout.addWidget(editButton)
         nameLayout.addWidget(deleteButton)
         self.nameLayout.addLayout(nameLayout)  
-
+        
+    def loadCharacterData(self, characterList):
+        for characterName, aliasList in characterList:
+            self.characters.append([characterName, aliasList])
+            self.addAliasRow(characterName)
+            
     def openNameDialog(self):
         dialog = NameDialog()
         if dialog.exec_() == QDialog.Accepted:  
@@ -67,8 +86,9 @@ class CharacterManager(QWidget):
             return name
     
     def deleteCharacter(self, name, layout):
-        if name in self.characters:
-            self.characters.remove(name)
+        i = next((i for i, character in enumerate(self.characters) if character[0] == name), -1)
+        if i != -1:
+            self.characters.pop(i)
             for i in reversed(range(layout.count())): 
                 widget = layout.itemAt(i).widget()
                 if widget is not None: 
@@ -77,9 +97,33 @@ class CharacterManager(QWidget):
         QMessageBox.information(self, "Remove Character", f"Removed {name}.")
     
     def editName(self, name):
-        self.characterMenu = speakerForm(name)
-        self.characterMenu.show()
-     
+        i = next((i for i, character in enumerate(self.characters) if character[0] == name), -1)
+        if i != -1:
+            characterEditDialog = speakerForm(self.characters[i])
+            if characterEditDialog.exec_() == QDialog.Accepted:  
+                character = characterEditDialog.getCharacter()
+                if character[0] != name:
+                    self.updateCharacterName(name, character[0])
+                self.characters[i] = character
+        else:
+            print("ERROR: Trying to edit character that doesn't exist") # TODO add actual error handling to this
+
+    def updateCharacterName(self, old_name, name):
+        i = next((i for i, layout in enumerate(self.nameLayout.children()) if layout.itemAt(0).widget().text() == old_name))
+        layout = self.nameLayout.itemAt(i)
+        
+        label = layout.itemAt(0).widget()
+        label.setText(name)
+        label.setAccessibleName(f"Name: {name}")
+        
+        editButton = layout.itemAt(1).widget()
+        editButton.clicked.disconnect()
+        editButton.clicked.connect(lambda: self.editName(name))
+        editButton.setAccessibleName(f"Edit {name} Button")
+        
+        deleteButton = layout.itemAt(2).widget()
+        deleteButton.clicked.disconnect()
+        deleteButton.clicked.connect(lambda: self.deleteCharacter(name, layout))
 
 class NameDialog(QDialog):
     def __init__(self):
@@ -117,8 +161,14 @@ class NameDialog(QDialog):
       
 
 if __name__ == "__main__":
+    ExampleCharacterList = [
+        ["Deckard", [["Deckard", "speed:1.0, pitch:1.0"], ["Rick",  "speed:1.0, pitch:1.1"]]],
+        ["Ripley",  [["Ripley",  "speed:1.0, pitch:1.0"], ["Ellen", "speed:1.2, pitch:0.9"]]],
+        ["Cooper",  [["Cooper",  "speed:1.0, pitch:1.0"], ["Joseph","speed:0.9, pitch:1.2"]]]
+    ]
     app = QApplication(sys.argv)
     editor = CharacterManager()
+    editor.loadCharacterData(ExampleCharacterList)
     editor.resize(300, 400)
     editor.show()
     sys.exit(app.exec_())
